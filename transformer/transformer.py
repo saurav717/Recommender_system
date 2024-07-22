04a7f9dfbf7074865dac992dac29198cb17f6406
saurav717 <saurav07@bu.edu>
2024-07-21 20:20:34 -0500

    target masking changes

--- transformer/transformer.py
+++ transformer/transformer.py
@@ -20,13 +20,13 @@ class transformer(nn.Module):
                  device :str="cuda",
                  max_length :int=64):
         super(transformer, self).__init__()
-        self.encoder = encoder(src_vocab_size,
-                               embed_size,
-                               num_layers,
-                               heads,
-                               ff_hidden_dim,
-                               max_seq_len,  
-                               dropout,
+        self.encoder = encoder(vocab_size=src_vocab_size,
+                               embed_size=embed_size,
+                               num_layers=num_layers,
+                               num_heads=heads,
+                               ff_hidden_dim=ff_hidden_dim,
+                               max_seq_length=max_seq_len,  
+                               dropout=dropout,
                                )
         self.decoder = Decoder(vocab_size=tgt_vocab_size,
                                embed_size=embed_size,
@@ -41,28 +41,32 @@ class transformer(nn.Module):
     
     def make_src_mask(self, src: torch.Tensor) -> torch.Tensor:
         src_mask = (src != self.src_pad_idx).unsqueeze(1).unsqueeze(2)
+        print("source mask shape = ", src_mask.shape)
         return src_mask 
     
     def make_tgt_mask(self, tgt: torch.Tensor) -> torch.Tensor:
         N, tgt_len = tgt.shape 
         tgt_mask = torch.tril(torch.ones((tgt_len, tgt_len))).expand(N, 1, tgt_len, tgt_len)
-        return tgt_mask 
+        padding_mask = (tgt != self.tgt_pad_idx).unsqueeze(1).unsqueeze(3)
+        return tgt_mask.bool().to(tgt.device) & padding_mask 
     
     def forward(self, src: torch.Tensor, tgt: torch.Tensor) -> torch.Tensor:
         src_mask = self.make_src_mask(src)
         tgt_mask = self.make_tgt_mask(tgt)
-        enc_src = self.encoder(x=src, mask=tgt_mask)
+        enc_src = self.encoder(x=src, mask=src_mask)
+        # print("transformer source shape = ", src.shape, " -- target shape = ", tgt.shape)
+        # print("transformer source mask  = ", src_mask.shape, " -- target mask = ", tgt_mask.shape)
         out = self.decoder(tgt, enc_src, src_mask, tgt_mask)
         return out 
         
 
-        
-        
-        
-    
-if __name__=="__main__":
-    text = "My name is Saurav"
-    tf = Transformer(text)
+# if __name__=="__main__":
+#     tgt = torch.randint(4,5,6)
+#     print("tgt = ", tgt.shape)
+#     N, tgt_len = tgt.shape 
+#     tgt_mask = torch.tril(torch.ones((tgt_len, tgt_len))).expand(N, 1, tgt_len, tgt_len)
+#     text = "My name is Saurav"
+#     tf = Transformer(text)
     
     # print(tf.attention_scores)
     
